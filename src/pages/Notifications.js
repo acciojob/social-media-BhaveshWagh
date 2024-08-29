b97616ed1b1f97a127a07c595d8a7dc0fbc22cc2
BhaveshWagh <bhaveshbm2711@gmail.com>
2024-08-29 10:09:57 +0530

    test 1.0

---
+++ src/pages/Notifications.js
@@ -0,0 +1,30 @@
+import React, { useContext } from "react";
+import { NotificationContext } from "../context/NotificationContext";
+import { UserContext } from "../context/UserContext";
+
+function Notifications() {
+  const { notifications } = useContext(NotificationContext);
+  const { users } = useContext(UserContext);
+
+  const getAuthor = (id) => {
+    return users.find((user) => user.id == id);
+  };
+
+  return (
+    <section className="container notifications">
+      <h2>Notifications</h2>
+      <section>
+        <div className="notificationsList">
+          {notifications.map((notification) => (
+            <section key={notification.id}>
+              <strong>{getAuthor(notification.authorId).name}</strong>{" "}
+              {notification.message}
+            </section>
+          ))}
+        </div>
+      </section>
+    </section>
+  );
+}
+
+export default Notifications;
